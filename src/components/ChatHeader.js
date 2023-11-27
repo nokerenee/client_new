6b0c84345cff99312fb268c5f94e8b87c5eacfec
jensiriphone <jennetsiriphone@gmail.com>
2023-11-27 14:38:51 +1300

    split into different components from chat.js

---
+++ src/components/ChatHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatHeader = () => {
+    return (
+        <div className="chat-header">
+            <p>Live Chat!</p>
+        </div>
+    );
+}
+
+export default ChatHeader;
